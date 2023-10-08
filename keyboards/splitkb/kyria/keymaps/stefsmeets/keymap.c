b3dd20bb5592fd9bddddb7a235c79bdd967cb33d
Stef Smeets <s.smeets@esciencecenter.nl>
2023-10-08 14:26:16 +0200

    Fix colemak layout

--- keyboards/splitkb/kyria/keymaps/stefsmeets/keymap.c
+++ keyboards/splitkb/kyria/keymaps/stefsmeets/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
       _______,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                        KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, _______,
       _______,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                        KC_M,    KC_N,    KC_E,    KC_I,    KC_O, _______,
-      _______,    KC_Z,    KC_X,    KC_D,    KC_V,    KC_B, _______, _______, _______, _______,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, _______,
+      _______,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, _______, _______, _______, _______,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
