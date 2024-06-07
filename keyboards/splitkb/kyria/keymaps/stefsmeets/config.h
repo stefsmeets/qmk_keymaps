662e0a2c5028c9578cb60c863452b6200ee8d21e
Stef Smeets <s.smeets@esciencecenter.nl>
2024-06-07 16:08:30 +0200

    Tweak tapping term

--- keyboards/splitkb/kyria/keymaps/stefsmeets/config.h
+++ keyboards/splitkb/kyria/keymaps/stefsmeets/config.h
@@ -1,31 +1,15 @@
-/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 350
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
-#define HOLD_ON_OTHER_KEY_PRESS
 #define PERMISSIVE_HOLD
+// #define HOLD_ON_OTHER_KEY_PRESS
 
 #define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
 
 // https://docs.qmk.fm/#/one_shot_keys
-#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TAP_TOGGLE 3
 #define ONESHOT_TIMEOUT 2500
