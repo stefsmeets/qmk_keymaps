8997ee2591c526798efc8a847f0ad262c8a2ef7d
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-03-04 20:09:21 +0100

    Add halcyon module

--- users/stefsmeets/rules.mk
+++ users/stefsmeets/rules.mk
@@ -33,3 +33,12 @@ KEY_OVERRIDE_ENABLE = yes
 
 # https://docs.qmk.fm/#/feature_dynamic_macros
 DYNAMIC_MACRO_ENABLE = no
+
+# Add this to your existing rules.mk if you have one
+ifneq ($(filter $(strip $(KEYBOARD)), splitkb/halcyon/kyria/rev4 \
+                                      splitkb/halcyon/ferris/rev1 \
+                                      splitkb/halcyon/lily58/rev2 \
+									  splitkb/halcyon/elora/rev2 \
+                                      splitkb/halcyon/corne/rev2),)
+	include $(USER_PATH)/splitkb/rules.mk
+endif
